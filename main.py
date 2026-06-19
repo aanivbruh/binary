6c45cb829c0fc9c4afdb90590665ae8577758932
Aaniv Chauhan <aaniv@outlook.com>
2026-06-19 12:22:11 -0800

    All done! addded number to binary conversion

--- main.py
+++ main.py
@@ -17,7 +17,23 @@ def convertToDecimal(string1: str):
     return num
 
 
+
+
+def convertToBinary(num:int):
+    ind = int(num ** 0.5)
+    str = ""
+    while ind >=0 :
+        if num >= 2 ** ind:
+            num -= 2 ** ind
+            str += "1"
+        else:
+            str += "0"
+        ind-=1
+    return str
+
 def addBinary(string1:str,string2:str):
-    return convertToDecimal(string1) + convertToDecimal(string2)
+    answer =  convertToDecimal(string1) + convertToDecimal(string2)
+    print(convertToBinary(answer))
+    return answer
 
 print(addBinary("1001","1"))
