2b6b7cc1a2adbc72a3329ce044bf8ea0eabfbe8c
Aaniv Chauhan <aaniv@outlook.com>
2026-06-19 17:41:37 -0800

    Added binary addition lolz

--- main.py
+++ main.py
@@ -17,8 +17,15 @@ def convertToDecimal(string1: str):
     return num
 
 def convertToBinary(num:int):
-    ind = int(num ** 0.5)
+    ind = 0
+    fakeNum = num
     str = ""
+
+    while fakeNum > 2:
+        new = fakeNum/2
+        fakeNum = new
+        ind+=1
+
     while ind >=0 :
         if num >= 2 ** ind:
             num -= 2 ** ind
@@ -26,11 +33,46 @@ def convertToBinary(num:int):
         else:
             str += "0"
         ind-=1
+    
     return str
 
 def addBinary(string1:str,string2:str):
     answer =  convertToDecimal(string1) + convertToDecimal(string2)
-    print(convertToBinary(answer))
     return answer
 
-print(addBinary("1001","00000001354242"))
+
+def addBinaryWBinary(string1:str,string2:str):
+   #longer string is string1, shorter is string 2
+   strResult = ""
+   carry = 0
+   if len(string1) < len(string2):
+       place = string1
+       string1 = string2
+       string2 = place
+   for x in range(len(string1)):
+       result = 0
+       num1 = string1[len(string1)-x-1:len(string1)-x]
+       num2 = string2[len(string1)-x-1:len(string1)-x]
+      
+       if num2 == "0" and num1 == "0":
+          result += 0
+       elif num2 == "0" and num1 == "1" or (num2 == "1"  and num1 == "0"):
+           result += 1
+       else:
+           result += 2
+
+       result += carry
+
+       if result < 2:
+           strResult = str(result) + strResult
+           carry = 0
+       else:
+          carry = 1
+          strResult =  str(result%2) + strResult
+   if carry == 1:
+       strResult = "1" + strResult
+   return strResult
+
+print(addBinaryWBinary("11111","10101"))
+    
+       
